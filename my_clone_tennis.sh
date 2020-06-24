e91b66625976f346cce62dfbcc9c353e4f9e23b8
Mirai Hattori <mirai2045@gmail.com>
2020-06-24 20:20:46 +0900

    update

--- my_clone_tennis.sh
+++ my_clone_tennis.sh
@@ -5,3 +5,4 @@ mkdir -p my_ws/src
 git clone --recursive https://github.com/future731/ball_orbit_estimator.git my_ws/src/ball_orbit_estimator
 git clone --recursive https://github.com/jsk-ros-pkg/jsk_control.git my_ws/src/jsk_control
 git clone --recursive https://github.com/future731/optmotiongen_sample.git my_ws/src/optmotiongen_sample
+git clone --recursive git@github.com:future731/tennis.git my_ws/src/tennis
