a88ee5f90f7f4bd5299ff18e3383c6a3f59808e7
Mirai Hattori <mirai2045@gmail.com>
2020-06-27 10:28:58 +0900

    remove configure_file from CMakeLists.txt (hrpsys_choreonoid_tutorials)

--- scripts-on-docker/my_launch_tennis.sh
+++ scripts-on-docker/my_launch_tennis.sh
@@ -3,4 +3,4 @@
 source /userdir/scripts-on-docker/cnoid_env.bash
 source /userdir/catkin_ws/devel/setup.bash
 apt-get purge -y ros-kinetic-openhrp3 ros-kinetic-hrpsys-ros-bridge
-rtmlaunch hrpsys_choreonoid_tutorials jaxon_red_choreonoid.launch TASK:=FOREHAND
+rtmlaunch tennis tennis.launch
