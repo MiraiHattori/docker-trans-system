055f495a8260198359e33c2c730c69a252ef56c7
Mirai Hattori <mirai2045@gmail.com>
2020-06-27 13:33:11 +0900

    update

--- scripts-on-docker/my_launch_tennis.sh
+++ scripts-on-docker/my_launch_tennis.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 source /userdir/scripts-on-docker/cnoid_env.bash
-source /userdir/catkin_ws/devel/setup.bash
+source /userdir/my_ws/devel/setup.bash
 apt-get purge -y ros-kinetic-openhrp3 ros-kinetic-hrpsys-ros-bridge
 rtmlaunch tennis tennis.launch
