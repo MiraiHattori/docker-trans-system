bbb8c6638a4e2a7484b1324b70ae8681bc4760cf
Mirai Hattori <m-hattori@jsk.imi.u-tokyo.ac.jp>
2020-07-10 21:55:08 +0900

    remove jsk_recognition

--- my_clone_tennis.sh
+++ my_clone_tennis.sh
@@ -4,6 +4,6 @@ git clone --recursive https://github.com/future731/ball_state_msgs.git catkin_ws
 mkdir -p my_ws/src
 git clone --recursive https://github.com/future731/ball_orbit_estimator.git my_ws/src/ball_orbit_estimator
 git clone --recursive https://github.com/jsk-ros-pkg/jsk_control.git my_ws/src/jsk_control
-git clone --recursive https://github.com/jsk-ros-pkg/jsk_recognition.git my_ws/src/jsk_recognition
+# git clone --recursive https://github.com/jsk-ros-pkg/jsk_recognition.git my_ws/src/jsk_recognition
 git clone --recursive https://github.com/future731/optmotiongen_sample.git my_ws/src/optmotiongen_sample
 git clone --recursive git@github.com:future731/tennis.git my_ws/src/tennis
