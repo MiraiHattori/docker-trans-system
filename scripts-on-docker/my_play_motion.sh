fa7130bfe908d7442a00343b8e9dda0dfe3d9b51
Mirai Hattori <mirai2045@gmail.com>
2020-09-29 17:38:03 +0900

    add play motion

---
+++ scripts-on-docker/my_play_motion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /userdir/scripts-on-docker/cnoid_env.bash
+source /userdir/my_ws/devel/setup.bash
+apt-get install -y ros-kinetic-eus-nlopt ros-kinetic-eus-qp ros-kinetic-eus-qpoases
+roscd tennis/euslisp
+roseus play-motion.l
