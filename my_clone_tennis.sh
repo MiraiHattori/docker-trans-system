15861d55f6326c8a45e05105d57d3d30960e9e03
Mirai Hattori <m-hattori@jsk.imi.u-tokyo.ac.jp>
2020-10-05 13:09:31 +0900

    fix log plotter clone path

--- my_clone_tennis.sh
+++ my_clone_tennis.sh
@@ -5,6 +5,6 @@ mkdir -p my_ws/src
 git clone --recursive https://github.com/future731/ball_orbit_estimator.git my_ws/src/ball_orbit_estimator
 git clone --recursive https://github.com/jsk-ros-pkg/jsk_control.git my_ws/src/jsk_control
 # git clone --recursive https://github.com/jsk-ros-pkg/jsk_recognition.git my_ws/src/jsk_recognition
-git clone --recursive https://github.com/kindsenior/log_plotter.git
+git clone --recursive https://github.com/kindsenior/log_plotter.git my_ws/src/log_plotter
 git clone --recursive https://github.com/future731/optmotiongen_sample.git my_ws/src/optmotiongen_sample
 git clone --recursive git@github.com:future731/tennis.git my_ws/src/tennis
